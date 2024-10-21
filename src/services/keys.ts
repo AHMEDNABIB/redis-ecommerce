43585faeecc3d1b38ac28e1e794e2b8a96e8fd8b
AHMEDNABIB <nabibpallab@gmail.com>
2024-10-21 19:37:57 +0600

    feat : add caching to the pages

---
+++ src/services/keys.ts
@@ -0,0 +1,1 @@
+export const pageCachekey = ( id: string) => `pagecache#${id}`
--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,29 @@
-export const getCachedPage = (route: string) => {};
+import { pageCachekey } from "$services/keys";
+import { client } from "$services/redis";
 
-export const setCachedPage = (route: string, page: string) => {};
+
+const cacheRoutes = ["/about", "/privacy", "/auth/signin", "/auth/signup"]
+
+export const getCachedPage = (route: string) => { 
+    if (cacheRoutes.includes(route)) {
+        // return client.get('pagecache#'+route)
+        return client.get(pageCachekey(route))
+    }
+
+    return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if (cacheRoutes.includes(route)) {
+        // return client.set('pagecache#' + route, page,
+        //     // {
+        //     //     EX:2
+        //     // }
+        // )
+        return client.set( pageCachekey(route), page,
+            // {
+            //     EX:2
+            // }
+        )
+    }
+};
