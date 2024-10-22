2a34b9a8afa14988b16ff3d7fe0fc7957332ca48
AHMEDNABIB <nabibpallab@gmail.com>
2024-10-22 11:40:03 +0600

    feat: get useby id is added

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -2,11 +2,14 @@ import { userskey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
-// import { serialize } from './items/serialize';
+
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+    const user = await client.hGetAll(userskey(id))
+    return deserialize(id, user)
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
     const id = genId();
@@ -20,5 +23,13 @@ const serialize = (user:CreateUserAttrs) => {
         username: user.username,
         password: user.password
 
+    }
+}
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+    return {
+        id,
+        username: user.username,
+        password: user.password
     }
 }
