89fff042637ea2a71a648221e0323d3862ee5eca
AHMEDNABIB <nabibpallab@gmail.com>
2024-10-22 11:32:38 +0600

    feat: add serilize function

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,1 +1,3 @@
-export const pageCachekey = ( id: string) => `pagecache#${id}`
+export const pageCachekey = (id: string) => `pagecache#${id}`
+
+export const userskey = (userId: string) => 'users#' + userId
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,24 @@
+import { userskey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+// import { serialize } from './items/serialize';
 
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+    const id = genId();
+    await client.hSet(userskey(id), serialize(attrs));
+
+    return id;
+};
+
+const serialize = (user:CreateUserAttrs) => {
+    return {
+        username: user.username,
+        password: user.password
+
+    }
+}
